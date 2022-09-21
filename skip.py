68539c46bd7517e19a9c276d5c983319f348bb8a
AinsleySnow <AinsleySnow@outlook.com>
2022-09-22 01:12:36 +0800

    now skip.py can work correctly

--- skip.py
+++ skip.py
@@ -1,18 +1,44 @@
-from copy import deepcopy
 from random import randint, random
+
+
+class dyn_table:
+    max_size: int
+    data: list
+
+    def __init__(self, msz: int) -> None:
+        self.max_size = msz
+        self.data = []
+    
+    def __getitem__(self, i: int):
+        if len(self.data) - 1 <= i and i < self.max_size:
+            self.data.extend([None] * (i - len(self.data) + 1))
+            return self.data[i]
+        elif i <= len(self.data) - 1:
+            return self.data[i]
+        raise KeyError()
+
+    def __setitem__(self, i: int, v) -> None:
+        if len(self.data) - 1 <= i and i < self.max_size:
+            self.data.extend([None] * (i - len(self.data) + 1))
+            self.data[i] = v
+            return
+        elif i <= len(self.data) - 1:
+            self.data[i] = v
+            return
+        raise KeyError()
 
 
 class node:
     key: int
     value: int
     level: int
-    forward: list
+    forward: dyn_table
     
-    def __init__(self, level: int, key: int, value: int) -> None:
+    def __init__(self, level: int, key: int, value: int, max_level: int) -> None:
         self.key = key
         self.value = value
         self.level = level
-        self.forward = []
+        self.forward = dyn_table(max_level)
 
 
 class skip:
@@ -20,10 +46,10 @@ class skip:
     level: int
     max_level: int
 
-    def __init__(self) -> None:
-        self.header = None
-        self.level = None
-        self.max_level = None
+    def __init__(self, h: node, l: int, ml: int) -> None:
+        self.header = h
+        self.level = l
+        self.max_level = ml
 
 
 def search(s: skip, key: int) -> node:
@@ -46,38 +72,39 @@ def random_level(p: float, max_level: int) -> int:
 
 
 def insert(s: skip, key: int, new_value: int) -> None:
-    update = [None] * s.level
+    update = dyn_table(s.max_level)
     x = s.header
     for i in range(s.level - 1, -1, -1):
-        while x.forward[i].key < key:
+        while x.forward[i] and x.forward[i].key < key:
             x = x.forward[i]
-        update[i] = deepcopy(x)
+        update[i] = x
     
     x = x.forward[0]
-    if x.key == key:
+    if x and x.key == key:
         x.value = new_value
     else:
         new_level = random_level(0.5, s.max_level)
         if new_level > s.level:
-            for i in range(s.level + 1, new_level + 1):
+            for i in range(s.level, new_level):
                 update[i] = s.header
             s.level = new_level
-        x = node(new_level, key, new_value)
+        x = node(new_level, key, new_value, s.max_level)
         for i in range(s.level):
-            x.forward[i] = update[i].forward[i]
-            update[i].forward[i] = x
+            if update[i]:
+                x.forward[i] = update[i].forward[i]
+                update[i].forward[i] = x
 
 
-def delete(s: skip, key: int, new_value: int) -> None:
-    update = [None] * s.level
+def delete(s: skip, key: int) -> None:
+    update = dyn_table(s.max_level)
     x = s.header
     for i in range(s.level - 1, -1, -1):
-        while x.forward[i].key < key:
+        while x.forward[i] and x.forward[i].key < key:
             x = x.forward[i]
-        update[i] = deepcopy(x)
+        update[i] = x
     
     x = x.forward[0]
-    if x.key == new_value:
+    if x and x.key == key:
         for i in range(s.level):
             if update[i].forward[i] != x:
                 break
@@ -88,15 +115,20 @@ def delete(s: skip, key: int, new_value: int) -> None:
 
 if __name__ == '__main__':
     data = [(randint(0, 100)) for i in range(10)]
-    skip_list = skip()
-    skip_list.header = node(-1, -1, -1)
-    skip_list.max_level = 16
-    skip_list.level = 1
+    skip_list = skip(node(-1, -1, -1, 16), 1, 16)
 
     for (i, d) in enumerate(data):
         insert(skip_list, i, d)
     x = skip_list.header
-    while x.forward.get(0):
+    while x:
         print(f'{x.key}: {x.value}')
         x = x.forward[0]
-    
+
+    print()
+
+    delete(skip_list, 2)
+    delete(skip_list, 5)
+    x = skip_list.header
+    while x:
+        print(f'{x.key}: {x.value}')
+        x = x.forward[0]
